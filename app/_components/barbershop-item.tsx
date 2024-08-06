5ca835657fc5e0a84680ba0cead5057d276d772b
MarlonChi <marlonchiodelli@hotmail.com>
2024-08-06 20:50:52 -0300

    feat: barbershop page init

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+import { StarIcon } from "lucide-react";
+
 import { Barbershop } from "@prisma/client";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
-import { StarIcon } from "lucide-react";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
@@ -33,8 +35,8 @@ export const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
-            Reservar
+          <Button variant="secondary" className="mt-3 w-full" asChild>
+            <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
